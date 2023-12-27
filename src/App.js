95f4c20e8c5ddc01adc484884d82132e7f411820
Mike Lee <pride81_@hotmail.com>
2023-12-28 01:31:13 +0900

    Changed texts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function handleClick() {
+    alert("yo");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is where all things begin.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="top" onClick={handleClick}>
+      CV - Mike Lee
     </div>
   );
 }
