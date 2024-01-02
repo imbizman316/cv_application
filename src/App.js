2a2361c42365c39b787313b393ca62284c709f83
Mike Lee <pride81_@hotmail.com>
2024-01-02 19:46:41 +0900

    Upgraded

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from "react";
+import Education from "./components/Education";
 
 function App() {
 
+  //-------------General---------------------------------//
   const [edit,setEdit] = React.useState(false);
-  const [name,setName] = React.useState("");
+  const [name,setName] = React.useState("Enter your name");
   const [nameEdit,setNameEdit] = React.useState(false);
-  const [email,setEmail] = React.useState("");
+  const [email,setEmail] = React.useState("Enter your email");
   const [emailEdit,setEmailEdit] = React.useState(false);
-  const [phone,setPhone] = React.useState("");
+  const [phone,setPhone] = React.useState("Enter your phone");
   const [phoneEdit,setPhoneEdit] = React.useState(false);
+
+  //-------------Education---------------------------------//
+  const [edu, setEdu] = React.useState([]);
+  const [eduCount, setEduCount] = React.useState(0);
+  //const [schoolReal, setSchool] = React.useState("");
+
+  const [test, setText] = React.useState([
+    {
+      id: 100,
+      text: ""
+    },
+    {
+      id: 200,
+      text: ""
+    }
+  ])
+
 
   function handleClick() {
     setEdit(!edit);
@@ -47,33 +66,117 @@ function App() {
     setPhone(value)
   }
 
+  function setEduCountFunc() {
+    const tempCount = eduCount + 1
+    setEduCount(tempCount);    
+  }
+
+  //---Add education----------------//
+  function addEducation() {        
+
+    setEduCountFunc()
+    const tempEdu = edu;
+    tempEdu.push({
+      id: eduCount,
+      school: "",
+      major: "",
+      begin_year: "",
+      end_year: "",
+      isEdit: true,
+    })
+    setEdu(tempEdu);    
+    
+  }
+
+  //---Delete education-------------//
+  function deleteEducation(id) {
+
+    let tempEdu = [];
+    
+    edu.forEach((one) => {
+      if (one.id !== id) {
+        tempEdu.push(one);
+      }
+    })
+
+    setEdu(tempEdu);
+  }
+
+  //---onChange Education Handler---------//
+
+  const changeEducation = (e) => {
+
+    console.log(e.target.name);
+    console.log(e.target.id);
+    console.log(e.target.value);
+
+    
+
+    if (e.target.name === "schoolName") {
+
+      let tempEdu = [];
+      
+      edu.forEach((one) => {
+        if (one.id === e.target.id) {
+          tempEdu.push({...one, school: e.target.value})
+        } else {
+          tempEdu.push(one)
+        }
+      })
+
+      setEdu(tempEdu);
+
+    }
+
+    
+    console.log(edu);
+
+  }
+
+  //---Education edit Mode On -------//
+  const editModeOn = (id) => {
+
+    let tempEdu = []
+
+    edu.forEach((one) => {
+      if (one.id === id) {
+        tempEdu.push({...one,isEdit:!one.isEdit})
+      } else {
+        tempEdu.push(one)
+      }
+    })
+
+    setEdu(tempEdu);
+    
+  }
+
   return (
     <>
       {/* --------------General Section-------------- */}
       <div>
+        
         <div className="top" onClick={handleClick} style={{backgroundColor:"grey", height:"100px"}}>
           {edit ? <>edit</> : <>no-edit</>}  
         </div>
-        <div>
+        <h1>
           CV - Mike Lee
-        </div>
-        <div>
-          <p style={{backgroundColor: "yellow"}} onClick={handleNameEdit}>
-            Name
-          </p>
-          <p style={{backgroundColor: "pink"}} onClick={handleEmailEdit}>
-            Email
-          </p>
-          <p style={{backgroundColor: "skyblue"}} onClick={handlePhoneEdit}>
-            Phone Number
-          </p>
-        </div>
+        </h1>
+
+        {/* //----------Change Test----------// */}
+        <input type="text" placeholder="school name" name="schoolName" value={test}/>
+
         {/* --------------General Display-------------- */}
-        <div>                
+        <h3>
+          General
+        </h3>                
+        <div style={{border: "1px solid black"}}>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC7LPfug8zNYc8fk6hc59Avfe6PZaOmVViFQ&usqp=CAU"/>
           {edit && nameEdit ?
-            <input type="text" value={name} onChange={(e)=>handleNameChange(e.target.value)}
-            onKeyDown={handleNameEditKey}
-            />              
+            <div>
+              <input type="text" value={name} onChange={(e)=>handleNameChange(e.target.value)}
+              onKeyDown={handleNameEditKey}
+              />   
+            </div>           
             :
             <p onDoubleClick={handleNameEdit}>
               {name}
@@ -81,9 +184,11 @@ function App() {
           }   
 
           {edit && emailEdit ?
-            <input type="text" value={email} onChange={(e)=>handleEmailChange(e.target.value)}
-            onKeyDown={handleEmailEdit}
-            />              
+            <div>
+              <input type="text" value={email} onChange={(e)=>handleEmailChange(e.target.value)}
+              onKeyDown={handleEmailEdit}
+              />   
+            </div>           
             :
             <p onDoubleClick={handleEmailEdit}>
               {email}
@@ -91,9 +196,11 @@ function App() {
           }
 
           {edit && phoneEdit ?
-            <input type="text" value={phone} onChange={(e)=>handlePhoneChange(e.target.value)}
-            onKeyDown={handlePhoneEdit}
-            />              
+            <div>
+              <input type="text" value={phone} onChange={(e)=>handlePhoneChange(e.target.value)}
+              onKeyDown={handlePhoneEdit}
+              /> 
+            </div>             
             :
             <p onDoubleClick={handlePhoneEdit}>
               {phone}
@@ -103,6 +210,44 @@ function App() {
       </div>
       
       {/* --------------Education Section-------------- */}
+      <h3>
+        Education
+      </h3>
+      <div style={{border: "1px solid black"}}>
+
+        <button onClick={
+
+          ()=>{addEducation()}
+
+          }>Add education</button>        
+        
+        {eduCount}
+        {edu.length > 0 && edu[edu.length - 1].id}        
+      </div>
+      <>
+        {edu.map((each) => {
+          return (
+            <div>                    
+              
+              <Education
+
+                id = {each.id}
+                school = {each.school}
+                major = {each.major}
+                begin_year = {each.begin_year}
+                end_year = {each.end_year}    
+                deleteEducation = {deleteEducation}
+                isEdit = {each.isEdit}
+                editModeOn = {editModeOn}
+                changeEducation = {changeEducation}                
+
+               />                
+            </div>
+              )
+            }
+          )
+        }
+        </>
     </>
   );
 }
---
+++ src/components/Education.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+export default function Education({id, school, major, begin_year, end_year, deleteEducation, isEdit, editModeOn, changeEducation, schoolReal}) {  
+
+  const [schooRealTime, setSchoolRealTime] = React.useState(school)
+  
+
+  return (
+    <div>
+      {id}
+      {isEdit ? 
+        <>
+          <input id={id} type="text" placeholder="school name" value={schooRealTime} name="schoolName" onChange={changeEducation}></input>
+          <input id={id} type="text" placeholder="major" value={major} name="major" onChange={changeEducation}></input>
+          <input id={id} type="text" placeholder="start-year" value={begin_year} name="start-year" onChange={changeEducation}></input>
+          <input id={id} type="text" placeholder="end-year" value={end_year} name="end-year" onChange={changeEducation}></input>
+        </>
+       : 
+        <>
+          <h5>School: {school}</h5> 
+          <h5>Major: {major}</h5> 
+          <h5>Started: {begin_year}</h5> 
+          <h5>Finished: {end_year}</h5> 
+        </>
+      }      
+      <button onClick={()=>editModeOn(id)}>Edit</button>
+      <button onClick={()=>deleteEducation(id)}>Delete</button>      
+      <p>{`is Edit is ${isEdit}`}</p>
+      <p>------------------------------------------</p>
+    </div>
+  )
+}
